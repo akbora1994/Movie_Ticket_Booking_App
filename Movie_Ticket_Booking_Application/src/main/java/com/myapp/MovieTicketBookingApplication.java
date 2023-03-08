feb7b3f9f696356e088e6ead2cc74243709e6c77
akbora1994 <borasteak877@gmail.com>
2023-03-08 16:33:07 +0530

    project updated

--- Movie_Ticket_Booking_Application/src/main/java/com/myapp/MovieTicketBookingApplication.java
+++ Movie_Ticket_Booking_Application/src/main/java/com/myapp/MovieTicketBookingApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
+@EnableSwagger2
 public class MovieTicketBookingApplication {
 
 	public static void main(String[] args) {
